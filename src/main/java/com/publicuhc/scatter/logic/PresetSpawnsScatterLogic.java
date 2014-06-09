024d7974a758d8f5201836ecdd60d5e8545c9481
HowdenG <graham_howden1@yahoo.co.uk>
2014-06-09 13:02:05 +0100

    preset spawn locations scatter logic

---
+++ src/main/java/com/publicuhc/scatter/logic/PresetSpawnsScatterLogic.java
@@ -0,0 +1,55 @@
+package com.publicuhc.scatter.logic;
+
+import com.publicuhc.scatter.ScatterParameters;
+import com.publicuhc.scatter.exceptions.ScatterConfigurationException;
+import com.publicuhc.scatter.exceptions.ScatterLocationException;
+import com.publicuhc.scatter.zones.DeadZone;
+import org.bukkit.Location;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class PresetSpawnsScatterLogic extends ScatterLogic {
+
+    private final List<Location> m_spawns;
+
+    public PresetSpawnsScatterLogic(Random random, List<Location> spawns) {
+        super(random);
+        m_spawns = spawns;
+    }
+
+    @Override
+    public Location getScatterLocation(List<DeadZone> deadZones, ScatterParameters parameters) throws ScatterLocationException, ScatterConfigurationException {
+        if(!parameters.isMaxAttemptsSet()) {
+            throw new ScatterConfigurationException();
+        }
+        List<Location> spawns = new ArrayList<Location>(m_spawns);
+
+        while(spawns.size() > 0) {
+            int randomIndex = getRandom().nextInt(spawns.size());
+
+            Location spawnLocation = spawns.get(randomIndex);
+
+            if(isLocationWithinDeadZones(spawnLocation, deadZones)) {
+                spawns.remove(spawnLocation);
+                continue;
+            }
+
+            return  spawnLocation;
+        }
+
+        //all the spawn points are in deadzones or no locations set
+        throw new ScatterLocationException();
+    }
+
+    @Override
+    public String getID() {
+        return "Preset Spawns";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Spawn on predefined spawn points";
+    }
+}
