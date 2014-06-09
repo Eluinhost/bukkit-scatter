f3329f98e042736fe1a98bf9cf3d5ef866dde6c7
HowdenG <graham_howden1@yahoo.co.uk>
2014-06-09 11:59:04 +0100

    add the centre to the parameters

--- src/main/java/com/publicuhc/scatter/ScatterParameters.java
+++ src/main/java/com/publicuhc/scatter/ScatterParameters.java
@@ -1,10 +1,13 @@
 package com.publicuhc.scatter;
 
+import org.bukkit.Location;
+
 public class ScatterParameters {
 
     private Double m_radius = null;
     private Double m_playerRadius = null;
     private Integer m_maxAttempts = null;
+    private Location m_centre = null;
 
     /**
      * @return the radius for the scatter
@@ -86,5 +89,28 @@ public class ScatterParameters {
     public ScatterParameters setMaxAttempts(int attempts) {
         m_maxAttempts = attempts;
         return this;
+    }
+
+    /**
+     * @return the centre if set, null otherwise
+     */
+    public Location getCentre() {
+        return m_centre;
+    }
+
+    /**
+     * @param location the centre for the new scatter
+     * @return this, for method chaining
+     */
+    public ScatterParameters setCentre(Location location) {
+        m_centre = location;
+        return this;
+    }
+
+    /**
+     * @return whether the centre has been set or not
+     */
+    public boolean isCentreSet() {
+        return m_centre != null;
     }
 }
