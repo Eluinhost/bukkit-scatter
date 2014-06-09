fce8b4853af513e24cdc76dbb0e175dd60a74072
HowdenG <graham_howden1@yahoo.co.uk>
2014-06-09 14:46:31 +0100

    player radius should be handled with deadzones

--- src/main/java/com/publicuhc/scatter/ScatterParameters.java
+++ src/main/java/com/publicuhc/scatter/ScatterParameters.java
@@ -10,7 +10,6 @@ import java.util.List;
 public class ScatterParameters {
 
     private Double m_radius = null;
-    private Double m_playerRadius = null;
     private Integer m_maxAttempts = null;
     private Location m_centre = null;
     private List<Material> m_materials = new ArrayList<Material>();
@@ -39,33 +38,6 @@ public class ScatterParameters {
      */
     public ScatterParameters setRadius(double radius) {
         m_radius = radius;
-        return this;
-    }
-
-    /**
-     * @return the player radius for the scatter
-     * @throws java.lang.IllegalStateException if player radius wasnt set, check if set with isPlayerRadiusSet
-     */
-    public double getPlayerRadius() {
-        if(m_playerRadius == null) {
-            throw new IllegalStateException();
-        }
-        return m_playerRadius;
-    }
-
-    /**
-     * @return whether the player radius has been set or not
-     */
-    public boolean isPlayerRadiusSet() {
-        return m_playerRadius != null;
-    }
-
-    /**
-     * @param radius the player radius to set the scatter for
-     * @return this, for chaining
-     */
-    public ScatterParameters setPlayerRadius(double radius) {
-        m_playerRadius = radius;
         return this;
     }
 
