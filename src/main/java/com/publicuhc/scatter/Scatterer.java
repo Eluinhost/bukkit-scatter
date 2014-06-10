c61d5f05d856378cda30476036789eceda088698
HowdenG <graham_howden1@yahoo.co.uk>
2014-06-10 09:19:41 +0100

    add a way to clear base deadzones + add a base deadzone from a list of players

--- src/main/java/com/publicuhc/scatter/Scatterer.java
+++ src/main/java/com/publicuhc/scatter/Scatterer.java
@@ -5,6 +5,7 @@ import com.publicuhc.scatter.logic.ScatterLogic;
 import com.publicuhc.scatter.zones.DeadZone;
 import com.publicuhc.scatter.zones.DeadZoneBuilder;
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
 
 import java.util.List;
 
@@ -17,6 +18,15 @@ public interface Scatterer {
      * @throws ScatterLocationException if a location wasn't able to be fetched
      */
     List<Location> getScatterLocations(int amount) throws ScatterLocationException;
+
+    /**
+     * Scatter the players provided.
+     * <p>Teleports players after fetching all the valid locations</p>
+     *
+     * @param players the players to scatter
+     * @throws ScatterLocationException
+     */
+    void scatterPlayers(List<Player> players) throws ScatterLocationException;
 
     void setLogic(ScatterLogic logic);
 
@@ -31,4 +41,18 @@ public interface Scatterer {
     void setBaseDeadZones(List<DeadZone> zones);
 
     void addBaseDeadZone(DeadZone zone);
+
+    void clearBaseDeadZones();
+
+    /**
+     * Adds a base deadzone for every player provided using the DeadZoneBuilder
+     * @param players the player to create deadzones for
+     */
+    void addDeadZonesForPlayers(List<Player> players);
+
+    /**
+     * Adds a base deadzone for every player online that isn't in the list provided using the DeadZoneBuilder
+     * @param players the players to not create a deadzone for
+     */
+    void addDeadZonesForPlayersNotInList(List<Player> players);
 }
